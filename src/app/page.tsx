39f7f59142601eeb8f638cb70d8903d4229f1de2
Eduardo dudu <eduardo_regin@hotmail.com>
2023-06-20 18:19:39 -0400

    alterações

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,22 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen flex bg-zinc-900 text-neutral-300">
-      <div className="bg-orange-800 w-1/3"></div>
+    <div className="relative h-screen flex bg-zinc-900 text-neutral-300 items-center justify-center">
+      <div style={{'flex': '35% 1'}} className="h-full bg-orange-800"></div>
 
-      <div className="">Eduardo Miguel</div>
-
-      <div className="border">
+      <div style={{left: '35%', transform: 'translateX(-50%)'}} className="absolute border">
         <Image
           src="/vercel.svg"
           alt="Vercel Logo"
-          className="dark:invert"
+          className="dark:invert m-auto"
           width={100}
           height={24}
           priority
         />
       </div>
+
+      <div style={{'flex': '65% 1'}} className="h-full">Eduardo Miguel</div>
+
     </div>
   );
 }
